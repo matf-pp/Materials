b6847af56cfe62634cad56a6cf3091976d93bc09
Milan Cugurovic <milan.c.cugurovic@oracle.com>
2025-07-13 23:34:39 +0200

    Minor reorder.

--- constraint/python/A_06.py
+++ constraint/python/A_06.py
@@ -1,30 +1,55 @@
-#   TWO
-#  +TWO
-#  ------
-#  FOUR
-#
-
+# Dati sistem nejednacina nema resenje, tj. metog getSolutions() vraca praznu listu
+# Ukoliko se za promenljivu W domen promeni na {1,...,100} sistem ce imati resenje
 import constraint
 
 problem = constraint.Problem()
-# Definisemo promenljive i njihove vrednosti
-problem.addVariables("TF",range(1,10))
-problem.addVariables("WOUR",range(10))
 
-# Definisemo ogranicenje za cifre
-def o(t, w, o, f, u, r):
-    if 2*(t*100 + w*10 + o) == f*1000 + o*100 + u*10 + r:
+# Dodajemo promenljivu X i definisemo njen domen
+problem.addVariable('X', range(1,111))
+
+# Dodajemo promenljivu Y i definisemo njen domen
+problem.addVariable('Y', range(1,52,2))
+
+domenZ = []
+domenW = []
+
+for i in range(1,11):
+    domenZ.append(i*10)
+    domenW.append(i**3)
+
+# Dodajemo promenljivu Z i definisemo njen domen
+problem.addVariable('Z', domenZ)
+
+# Dodajemo promenljivu W i definisemo njen domen
+problem.addVariable('W', domenW)
+
+# Za ovako definisan domen za promenljivu W sistem ce imati resenje
+# problem.addVariable('W', range(1,101))
+
+
+def o1(x,w):
+    if x >= 2*w:
         return True
 
-# Dodajemo ogranicenja za cifre
-problem.addConstraint(o,"TWOFUR")
-# Dodajemo ogranicenje da su sve cifre razlicite
-problem.addConstraint(constraint.AllDifferentConstraint())
+def o2(y,z):
+    if 3 + y <= z:
+        return True
+
+def o3(x,y,z,w):
+    if x - 11*w + y + 11*z <= 100:
+        return True;
+
+# Dodajemo ogranicenja
+problem.addConstraint(o1, 'XW')
+problem.addConstraint(o2, 'YZ')
+problem.addConstraint(o3, 'XYZW')
 
 resenja = problem.getSolutions()
-
-for r in resenja:
-    print("-----")
-    print("  "+str(r['T'])+str(r['W'])+str(r['O']))
-    print(" +"+str(r['T'])+str(r['W'])+str(r['O']))
-    print("="+str(r['F'])+str(r['O'])+str(r['U'])+str(r['R']))
+# Proveravamo da li postoji resenje za sistem nejednacina
+if resenja==[]:
+    print("Sistem nema resenje.")
+else:
+    for r in resenja:
+        print("------------------------")
+        print("X = {0:d} , Y = {1:d} , Z = {2:d}, W = {3:d}".format(r['X'],r['Y'],r['Z'], r['W']))
+        print("------------------------")
