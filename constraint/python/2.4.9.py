c9c8885e5f5efe7a3cfb50a2380cb46d820fbf68
Ivan Ristovic <work@ristovic.net>
2026-06-14 00:13:16 +0200

    Fix constraint packing example

--- constraint/python/2.4.9.py
+++ constraint/python/2.4.9.py
@@ -2,7 +2,7 @@ from constraint import Problem
 
 def main():
     p = Problem(); p.addVariable('V', range(10)); p.addVariable('P', range(10)); p.addVariable('C', range(10))
-    p.addConstraint(lambda v,p,c: 4*v + 3*p + 2*c <= 35, ['V','P','C'])
+    p.addConstraint(lambda v,p,c: 4*v + 3*p + 2*c <= 9, ['V','P','C'])
     r = max(p.getSolutions(), key=lambda x: 15*x['V'] + 10*x['P'] + 7*x['C'])
-    print(f"Vino: {r['V']}, Parfem: {r['P']}, Casa: {r['C']}")
+    print(f"Viski: {r['V']}, Parfem: {r['P']}, Casa: {r['C']}")
 if __name__ == '__main__': main()
